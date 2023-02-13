4892ab1cc91016917b880e2dedeb3819ad027034
Fat Buddha Designs <74487846+fat-buddha-designs@users.noreply.github.com>
2023-02-13 07:40:54 +0000

    Kirby to 3.9.1

--- site/plugins/enhanced-toolbar-link-dialog/src/index.js
+++
@@ -1,9 +0,0 @@
-import EnhancedToolbarLinkDialog from "./components/EnhancedToolbarLinkDialog";
-
-panel.plugin('gearsdigital/enhanced-toolbar-link-dialog', {
-  components: {
-    'k-toolbar-link-dialog': {
-      extends: EnhancedToolbarLinkDialog
-    }
-  }
-});
